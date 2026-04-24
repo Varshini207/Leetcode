a002deb48d1612e8fa6ebcb62f2c22208dcd9c7f
Varshini207 <168453378+Varshini207@users.noreply.github.com>
2026-04-24 07:02:20 +0530

    Time: 1 ms (64.11%), Space: 19.7 MB (79.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -2,9 +2,7 @@ class Solution(object):
     def reverseString(self,s):
         l=0
         r=len(s)-1
-        while l<(len(s)/2)+1 and r>(len(s)/2)-1:
-            t=s[l]
-            s[l]=s[r]
-            s[r]=t    
+        while l<r:
+            s[l],s[r]=s[r],s[l]
             l+=1
             r-=1 
